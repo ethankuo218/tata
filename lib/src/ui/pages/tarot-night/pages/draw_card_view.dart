f2f5471e3bcc6d2f111a6e34f0309ac0e83fcf3a
ethankuo218 <ethankuo0218@gmail.com>
2024-07-04 00:44:12 +0800

    fix: refactor draw card view

--- lib/src/ui/pages/tarot-night/pages/draw_card_view.dart
+++ lib/src/ui/pages/tarot-night/pages/draw_card_view.dart
@@ -1,8 +1,11 @@
-import 'dart:ui';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_floating_bottom_bar/flutter_floating_bottom_bar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:tata/src/core/providers/pages/tarot-night/draw_card_view_provider.dart';
 import 'package:tata/src/ui/pages/tarot-night/pages/test_result_view.dart';
 
@@ -19,75 +22,184 @@ class TarotNightDrawCardView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(tarotNightDrawCardViewProvider).when(
           data: (card) => Scaffold(
-            appBar: AppBar(
-              title: const Text('Draw Card'),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(50),
-              child: Column(
-                children: [
-                  const Text(
-                    '在您抽牌前，請先做一次深呼吸，讓心靈平靜下來。',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                  const SizedBox(height: 16),
-                  const Text('閉上眼睛，想想您想問的問題以及相關的人事物，與塔羅牌卡做深層的連結。',
-                      style: TextStyle(fontSize: 16, color: Colors.white)),
-                  const SizedBox(height: 16),
-                  const Text('最後，將眼睛睜開，輕輕抽取一張，讓直覺引領您走向答案。',
-                      style: TextStyle(fontSize: 16, color: Colors.white)),
-                  const SizedBox(height: 60),
-                  Stack(
-                    children: [
-                      Container(
-                        width: 200,
-                        height: 250,
-                        decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 137, 118, 82)
-                                .withOpacity(0.3),
-                            border: Border.all(
-                                color: const Color.fromARGB(255, 137, 118, 82)
-                                    .withOpacity(0.6),
-                                width: 2),
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                      Container(
-                        width: 200,
-                        height: 250,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Image.asset(
-                          'assets/images/star.png',
-                          opacity: const AlwaysStoppedAnimation(0.5),
-                        ),
-                      ),
-                      BackdropFilter(
-                          blendMode: BlendMode.overlay,
-                          filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-                          child: Container(
-                            width: 200,
-                            height: 250,
-                            decoration: BoxDecoration(
-                                color: Colors.white.withOpacity(0.0)),
-                          ))
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {
-                      ref
-                          .read(tarotNightDrawCardViewProvider.notifier)
-                          .drawCard(roomId: roomId, question: question)
-                          .then((value) => context.pushReplacement(
-                              TarotNightTestResultView.routeName,
-                              extra: roomId));
-                    },
-                    child: const Text('選中此牌'),
-                  ),
-                ],
+              appBar: AppBar(
+                backgroundColor: const Color.fromARGB(255, 12, 13, 32),
+                leading: const SizedBox.shrink(),
               ),
-            ),
-          ),
+              body: BottomBar(
+                  offset: 30,
+                  barColor: Colors.transparent,
+                  child: SizedBox(
+                      height: 48,
+                      width: 160.5,
+                      child: FittedBox(
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 223, 130, 255),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 12)),
+                          onPressed: () {
+                            ref
+                                .read(tarotNightDrawCardViewProvider.notifier)
+                                .drawCard(roomId: roomId, question: question)
+                                .then((value) => context.pushReplacement(
+                                    TarotNightTestResultView.routeName,
+                                    extra: roomId));
+                          },
+                          child: const Text('查看結果',
+                              style: TextStyle(
+                                  height: 1.0,
+                                  color: Color.fromARGB(255, 12, 13, 32),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500)),
+                        ),
+                      )),
+                  body: (context, controller) => Column(
+                        children: [
+                          Expanded(
+                            child: Container(
+                                width: double.infinity,
+                                padding:
+                                    const EdgeInsets.fromLTRB(16, 20, 16, 36),
+                                decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(255, 12, 13, 32),
+                                        Color.fromARGB(255, 26, 0, 58),
+                                      ],
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter),
+                                ),
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      SvgPicture.asset(
+                                        'assets/images/star_5.svg',
+                                        width: 40,
+                                        height: 40,
+                                      ),
+                                      const SizedBox(height: 24),
+                                      const Text(
+                                        '在您抽牌前，請先做一次深呼吸，\n讓心靈平靜下來。',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          height: 7 / 5,
+                                          color: Color.fromARGB(
+                                              255, 241, 198, 255),
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 40),
+                                      const Text(
+                                        '閉上眼睛，\n想想您想問的問題以及相關的人事物，\n與塔羅牌卡做深層的連結。',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          height: 14 / 9,
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 40),
+                                      Stack(
+                                        children: List.generate(
+                                            10,
+                                            (index) => Transform.rotate(
+                                                angle: (pi / 180) *
+                                                    (72 - index * 8),
+                                                child: Opacity(
+                                                    opacity: (index + 1) * 0.1,
+                                                    child: Container(
+                                                      width: 200,
+                                                      height: 280,
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              16),
+                                                      decoration: BoxDecoration(
+                                                          color:
+                                                              const Color
+                                                                  .fromARGB(255,
+                                                                  39, 30, 61),
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(20),
+                                                          border:
+                                                              GradientBoxBorder(
+                                                                  gradient:
+                                                                      LinearGradient(
+                                                                    colors: [
+                                                                      const Color
+                                                                              .fromARGB(
+                                                                              255,
+                                                                              223,
+                                                                              130,
+                                                                              255)
+                                                                          .withOpacity(
+                                                                              0.8),
+                                                                      const Color
+                                                                              .fromARGB(
+                                                                              255,
+                                                                              241,
+                                                                              198,
+                                                                              255)
+                                                                          .withOpacity(
+                                                                              0.2),
+                                                                      const Color
+                                                                              .fromARGB(
+                                                                              255,
+                                                                              223,
+                                                                              130,
+                                                                              255)
+                                                                          .withOpacity(
+                                                                              0.8),
+                                                                      const Color
+                                                                              .fromARGB(
+                                                                              255,
+                                                                              241,
+                                                                              198,
+                                                                              255)
+                                                                          .withOpacity(
+                                                                              0.2),
+                                                                    ],
+                                                                    begin: Alignment
+                                                                        .topLeft,
+                                                                    end: Alignment
+                                                                        .bottomRight,
+                                                                  ),
+                                                                  width: 2)),
+                                                      child: Column(
+                                                        children: [
+                                                          SvgPicture.asset(
+                                                            'assets/images/star_2.svg',
+                                                            width: 20,
+                                                            height: 20,
+                                                            colorFilter:
+                                                                const ColorFilter
+                                                                    .mode(
+                                                                    Color.fromARGB(
+                                                                        255,
+                                                                        223,
+                                                                        130,
+                                                                        255),
+                                                                    BlendMode
+                                                                        .srcIn),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    )))),
+                                      )
+                                    ],
+                                  ),
+                                )),
+                          )
+                        ],
+                      ))),
           loading: () => const Center(child: CircularProgressIndicator()),
           error: (error, stack) => Text('Error: $error'),
         );
