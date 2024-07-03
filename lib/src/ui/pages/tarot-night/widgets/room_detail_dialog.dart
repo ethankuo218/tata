6b2aed37bb711c168aada5d8a312f9680f40ee97
ethankuo218 <ethankuo0218@gmail.com>
2024-07-04 00:18:13 +0800

    fix: refactor room info dialog

--- lib/src/ui/pages/tarot-night/widgets/room_detail_dialog.dart
+++ lib/src/ui/pages/tarot-night/widgets/room_detail_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tata/src/core/models/tarot_night_room.dart';
 
 Future<Object?> showTarotNightRoomDetailDialog(BuildContext context,
@@ -10,26 +12,34 @@ Future<Object?> showTarotNightRoomDetailDialog(BuildContext context,
       style: const TextStyle(),
       child: Center(
         child: Container(
-          height: 400,
+          constraints: const BoxConstraints(minHeight: 400, maxHeight: 600),
           margin: const EdgeInsets.symmetric(horizontal: 20),
           padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
               color: const Color.fromARGB(255, 12, 13, 32),
-              border:
-                  Border.all(color: const Color.fromARGB(255, 241, 198, 255)),
+              border: Border.all(
+                  color: const Color.fromARGB(255, 241, 198, 255), width: 2),
               borderRadius: const BorderRadius.all(Radius.circular(28))),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: const FaIcon(
+                  GestureDetector(
+                    onTap: () {
+                      context.pop();
+                    },
+                    child: const SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: FaIcon(
                         FontAwesomeIcons.xmark,
                         color: Colors.white,
-                      )),
+                        size: 20,
+                      ),
+                    ),
+                  )
                 ],
               ),
               const SizedBox(height: 20),
@@ -42,18 +52,113 @@ Future<Object?> showTarotNightRoomDetailDialog(BuildContext context,
                     fontWeight: FontWeight.w400),
               ),
               const SizedBox(height: 20),
-              Text(
-                roomInfo.description,
-                maxLines: 8,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    height: 12 / 7,
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                    SvgPicture.asset(
+                      "assets/images/star_3.svg",
+                      width: 20,
+                      height: 20,
+                      colorFilter: ColorFilter.mode(
+                          Colors.white.withOpacity(0.4), BlendMode.srcIn),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text('主題：',
+                        style: TextStyle(
+                            height: 1.15,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400)),
+                    const SizedBox(width: 8),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 2),
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 241, 198, 255)
+                              .withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Text(
+                        TarotNightRoomTheme.toText(roomInfo.theme),
+                        style: const TextStyle(
+                            height: 12 / 7,
+                            color: Color.fromARGB(255, 241, 198, 255),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ]),
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                    SvgPicture.asset(
+                      "assets/images/star_3.svg",
+                      width: 20,
+                      height: 20,
+                      colorFilter: ColorFilter.mode(
+                          Colors.white.withOpacity(0.4), BlendMode.srcIn),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text('人數：',
+                        style: TextStyle(
+                            height: 1.15,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400)),
+                    const SizedBox(width: 8),
+                    const FaIcon(
+                      FontAwesomeIcons.solidUser,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      size: 16,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      roomInfo.memberCount.toString(),
+                      style: const TextStyle(
+                          height: 1.15,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400),
+                    )
+                  ]),
+                ],
               ),
-              const Spacer(),
+              const SizedBox(height: 20),
+              Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                SvgPicture.asset(
+                  "assets/images/star_3.svg",
+                  width: 20,
+                  height: 20,
+                  colorFilter: ColorFilter.mode(
+                      Colors.white.withOpacity(0.4), BlendMode.srcIn),
+                ),
+                const SizedBox(width: 4),
+                const Text('房主的心事分享',
+                    style: TextStyle(
+                        height: 1.15,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400)),
+              ]),
+              const SizedBox(height: 20),
+              Container(
+                constraints: const BoxConstraints(minHeight: 120),
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 241, 198, 255)
+                        .withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(16)),
+                child: Text(
+                  roomInfo.description,
+                  maxLines: 12,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      height: 12 / 7,
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
               const SizedBox(height: 20),
               ElevatedButton(
                   style: ElevatedButton.styleFrom(
