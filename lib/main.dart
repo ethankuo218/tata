4f26143e27c2993dd4d83f636186a7d4767a5fb2
ethankuo218 <ethankuo0218@gmail.com>
2024-08-05 08:20:12 +0800

    separate dev and prod environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:tata/flavors.dart';
 import 'package:tata/src/core/services/push_notifications_service.dart';
 
-import 'firebase_options.dart';
+import 'firebase_options_dev.dart' as firebase_dev_options;
+import 'firebase_options_dev.dart' as firebase_prod_options;
 import 'src/app.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-void main() async {
+FutureOr<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  FirebaseOptions options = F.appFlavor == Flavor.dev
+      ? firebase_dev_options.DefaultFirebaseOptions.currentPlatform
+      : firebase_prod_options.DefaultFirebaseOptions.currentPlatform;
+
+  await Firebase.initializeApp(name: F.appFlavor!.name, options: options);
 
   // initialize Firebase Messaging
   await PushNotificationsService.init();
